8e3ab3eeb93000f05e1ff610872f9102e08f85f9
Kitessa Fikadu <kitessafikadu@gmail.com>
2026-02-14 23:29:28 +0300

    feat: initialize database and set up debt routes in main function

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	httpdelivery "expense_tracker/delivery/http"
 	"expense_tracker/infrastructure/db"
-	"fmt"
+	infrarepo "expense_tracker/infrastructure/repository"
+	"expense_tracker/usecases"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Mock server running ...")
-	db.DB_Init()
+	if err := db.DB_Init(); err != nil {
+		log.Fatalf("failed to initialize database: %v", err)
+	}
+
+	debtRepo := infrarepo.NewDebtRepositoryPG(db.DB)
+	debtUsecase := usecases.NewDebtUsecase(debtRepo)
+	debtHandler := httpdelivery.NewDebtHandler(debtUsecase)
+
+	mux := http.NewServeMux()
+	httpdelivery.RegisterDebtRoutes(mux, debtHandler)
+
+	log.Println("Server running on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("server stopped: %v", err)
+	}
 }
